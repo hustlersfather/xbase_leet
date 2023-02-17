e15e80fe339ba3f22331051dba42bbe14b17f542
hustlersfathers <75556276+hustlersfather@users.noreply.github.com>
2023-02-17 11:20:46 +0100

    Update index.php

--- index.php
+++ index.php
@@ -70,79 +70,6 @@ $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
             window.FontAwesomeConfig = { autoReplaceSvg: false }
         </script>
          <script type="text/javascript">
-             function ajaxinfo() {
-                $.ajax({
-                    type: 'GET',
-                    url: 'ajaxinfo.html',
-                    timeout: 10000,
-
-                    success: function(data) {
-                        if (data != '01') {
-                            var data = JSON.parse(data);
-                            for (var prop in data) {
-                                $("#" + prop).html(data[prop]).show();
-                            }
-                        } else {
-                            window.location = "logout.html";
-                        }
-                    }
-                });
-
-            }
-            setInterval(function() {
-                ajaxinfo()
-            }, 3000);
-
-            ajaxinfo();
-
-$(document).keydown(function(event){
-    if(event.which=="17")
-        cntrlIsPressed = true;
-});
-
-$(document).keyup(function(){
-    cntrlIsPressed = false;
-});
-
-var cntrlIsPressed = false;
-
-
-function pageDiv(n,t,u,x){
-  if(cntrlIsPressed){
-    window.open(u, '_blank');
-    return false;
-  }
-        var obj = { Title: t, Url: u };
-        if ( ("/"+obj.Url) != location.pathname) {
-        	if (x != 1) {history.pushState(obj, obj.Title, obj.Url);}
-        	else{history.replaceState(obj, obj.Title, obj.Url);}
-
-    	}
-      document.title = obj.Title;
-    $("#mainDiv").html('<div id="mydiv"><img src="files/img/load2.gif" class="ajax-loader"></div>').show();
-    $.ajax({
-    type:       'GET',
-    url:        'divPage'+n+'.html',
-    success:    function(data)
-    {
-        $("#mainDiv").html(data).show();
-        newTableObject = document.getElementById('table');
-        sorttable.makeSortable(newTableObject);
-        $(".sticky-header").floatThead({top:60});
-        if(x==0){ajaxinfo();}
-      }});
-    if (typeof stopCheckBTC === 'function') { 
-    var a = stopCheckBTC();
-     }
-
-}
-
-$(window).on("popstate", function(e) {
-        location.replace(document.location);
-
-});
-
-
 function setTooltip(btn, message) {
   console.log("hide-1");
   $(btn).tooltip('hide')
@@ -150,10 +77,9 @@ function setTooltip(btn, message) {
     .tooltip('show');
      console.log("show");
 }
-
-                  function hideTooltip(btn) {
-                  setTimeout(function() 
-    {$(btn).tooltip('hide'); console.log("hide-2");}, 1000);}
+function hideTooltip(btn) {
+setTimeout(function() 
+           {$(btn).tooltip('hide'); console.log("hide-2");}, 1000);}
 </script>
         <style>@import url(https://fonts.googleapis.com/css?family=Roboto:400);
             .navbar-nav .dropdown-menu
